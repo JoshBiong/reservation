809c967966d8a6891a240bcb7bd1ee534cf6b924
JoshBiong <joshuabiong7@gmail.com>
2022-07-01 12:15:31 +0800

    admin user

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () { 
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+
+});
+
+require __DIR__.'/auth.php';
